f7b95b2185643fc66456fb7b007d98585a4bcf63
HT-35 <huyfa352002@gmail.com>
2024-04-24 23:52:33 +0700

    save

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 const cors = require("cors");
 
 const exampleProxy = createProxyMiddleware({
-  target: "http://221.132.33.175:3000", // target host with the same base path
+  target: "http://localhost:3000", // target host with the same base path
   changeOrigin: true, // needed for virtual hosted sites
 });
 app.use("/", exampleProxy);
